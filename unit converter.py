a582bb40f24bb6f592a5be93c27c8aaf4ccdd1d5
fogolo <islamdinom71@gmail.com>
2025-03-19 02:14:49 +0100

    Create unit converter.py

---
+++ unit converter.py
@@ -0,0 +1,10 @@
+def km_to_miles(km):
+    return km * 1.609344
+kilom =float(input("write ur km that u want to transfer to km here:"))
+mile=km_to_miles(kilom)
+print(f"{kilom} km is equal to {mile:.2f} miles ")
+def kg_to_pounds(kg):
+    return kg * 2.20462
+kg = float(input("write ur kg that u want to transfer to km here:"))
+pound = kg_to_pounds(kg)
+print(f"{kg} kg is equal to {pound:.2f} pounds ")
